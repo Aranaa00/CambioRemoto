07f27670b00568e0dbaf9c4ba7d95b55f7e8d77b
sergioGN04 <sergio.gonzalez.noria.alu@iesfernandoaguilar.es>
2024-03-07 16:17:13 +0100

    Modificacion alumnoB

--- Persona.java
+++ Persona.java
@@ -1,4 +1,5 @@
 public class Persona {
 	System.out.println("Esto es una prueba");
-	System.out.println("Esto es un cambio de nuestro proyecto");	
+	System.out.println("Esto es un cambio de nuestro proyecto");
+	System.out.println("Modificacion alumnoB clase Persona");
 }
